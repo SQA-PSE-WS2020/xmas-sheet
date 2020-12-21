219a79725e041031b32e2f6691916822bcce5b87
Dudrie <Dudrie@gmx.de>
2020-12-21 20:27:03 +0100

    Add more boilerplate to decrease the entry hurdle.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,6 @@ open module tictactoe {
     requires javafx.base;
     requires transitive javafx.graphics;
 
-    exports de.unistuttgart.iste.rss.oo.tictactoe;
+    exports de.unistuttgart.iste.rss.oo.tictactoe.main;
+    exports de.unistuttgart.iste.rss.oo.tictactoe.logic;
 }
